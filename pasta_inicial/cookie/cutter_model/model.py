dfe4455cb424683eb4bde16b692fb4e1115a8e5d
v <xultezz@gmail.com>
2024-01-18 15:41:23 -0300

    .

--- pasta_inicial/cookie/cutter_model/model.py
+++
@@ -1,4 +0,0 @@
-def simple_model(x: int) -> int:
-    """Just for example purposes."""
-    x = 1
-    return x
--- pasta_inicial/cookie/package_manager/__init__.py
+++
@@ -1,3 +0,0 @@
-from .requirements import get_requirements
-
-__all__ = ["get_requirements"]
